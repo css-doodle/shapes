5887a20a89eb5223faf86d89439138a5e28d3ebb
yuanchuan <yuanchuan23@gmail.com>
2021-05-13 23:19:13 +0800

    Upgrade docs, packages and rebuild

--- Makefile
+++
@@ -1,5 +0,0 @@
-build:
-	@npm run build
-
-ship:
-	@git subtree push --prefix dist origin gh-pages
--- src/index.js
+++
@@ -1,8 +0,0 @@
-import 'css-doodle';
-import './basic.css';
-
-import App from './app.svelte';
-
-new App({
-  target: document.getElementById('app')
-});
