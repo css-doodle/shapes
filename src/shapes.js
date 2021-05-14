2fa61f557e697a1a30dc3cf1eca2f998621e5719
yuanchuan <yuanchuan23@gmail.com>
2021-05-14 12:38:43 +0800

    update shape list

--- src/shapes.js
+++ src/shapes.js
@@ -1,36 +1,43 @@
 export default read(`
 
 #heptagon
-	points: 7;
+  points: 7;
 
 
 #star
-	points: 10;
-	r: seq(1, .618);
+  points: 10;
+  r: seq(1, .618);
 
 
 #another star
-	points: 10;
-	r: cos(5t);
+  points: 10;
+  r: cos(5t);
 
 
 #star with frame
-	points: 10;
-	r: cos(5t);
-	frame: 8;
+  points: 10;
+  r: cos(5t);
+  frame: 8;
+
+
+#frames
+  split: 40;
+  x: cos(2t)^2 * cos(5t);
+  y: cos(2t)^2 * sin(5t);
+  frame: 2;
 
 
 #windwheel
-	points: 18;
-	R: seq(.618, 1, 0);
-	T: seq(t+.55, t, t);
-	x: R * cos(T);
-	y: R * sin(T);
+  points: 18;
+  R: seq(.618, 1, 0);
+  T: seq(t+.55, t, t);
+  x: R * cos(T);
+  y: R * sin(T);
 
 
 #cross
-	points: 20;
-	r: cos(2t);
+  points: 20;
+  r: cos(2t);
 
 
 #infinity
@@ -86,19 +93,19 @@ export default read(`
 
 
 #heart
-	points: 100;
-	scale: .4;
-	x: sin(2t) + sin(t) * 1.2;
-	y: cos(t) + cos(2t) * 1.2 - .3;
+  points: 100;
+  scale: .4;
+  x: sin(2t) + sin(t) * 1.2;
+  y: cos(t) + cos(2t) * 1.2 - .3;
 
 
 #heart2
-	points: 2400;
-	scale: .34;
-	origin: 0 -1.6;
-	rotate: 180;
-	s: sqrt.abs.cos(t) / (sin(t) + 1.6);
-	r: 2 + (s - 2) * sin(t);
+  points: 2400;
+  scale: .34;
+  origin: 0 -1.6;
+  rotate: 180;
+  s: sqrt.abs.cos(t) / (sin(t) + 1.6);
+  r: 2 + (s - 2) * sin(t);
 
 
 #heart3
@@ -111,159 +118,159 @@ export default read(`
 
 
 #four-leaf clover
-	points: 200;
-	r: sin(2t);
+  points: 200;
+  r: sin(2t);
 
 
 #clover
-	points: 300;
-	r: cos(4t);
+  points: 300;
+  r: cos(4t);
 
 
 #gear
-	points: 300;
-	scale: .7;
-	r: cos.cos.cos.cos.cos(7t) ^ cos(7t);
+  points: 300;
+  scale: .7;
+  r: cos.cos.cos.cos.cos(7t) ^ cos(7t);
 
 
 #butterfly
-	points: 30;
-	r: cos(4t) * sin(2t);
+  points: 30;
+  r: cos(4t) * sin(2t);
 
 
 #butterfly 2
-	points: 400;
-	x: sin(2t) * sin(3t) * sin(5t);
-	y: cos(t) * cos(8t)^2;
+  points: 400;
+  x: sin(2t) * sin(3t) * sin(5t);
+  y: cos(t) * cos(8t)^2;
 
 
 #css-doodle logo
-	fill-rule: evenodd;
-	points: 400;
-	x: cos(5t)^2 * sin(t);
-	y: sin(2t)^2 * cos(5t);
+  fill-rule: evenodd;
+  points: 400;
+  x: cos(5t)^2 * sin(t);
+  y: sin(2t)^2 * cos(5t);
 
 
 #butterfly 3
-	fill-rule: evenodd;
-	points: 400;
-	x: cos(2t) * cos(5t) * sin(t);
-	y: sin(2t) * sin(4t) * cos(5t);
+  fill-rule: evenodd;
+  points: 400;
+  x: cos(2t) * cos(5t) * sin(t);
+  y: sin(2t) * sin(4t) * cos(5t);
 
 
 #flower
-	fill-rule: evenodd;
-	points: 300;
-	scale: .45;
-	x: cos(2t) + cos(π - 7t);
-	y: sin(2t) + sin(7t);
+  fill-rule: evenodd;
+  points: 300;
+  scale: .45;
+  x: cos(2t) + cos(π - 7t);
+  y: sin(2t) + sin(7t);
 
 
 #flower 2
-	fill-rule: evenodd;
-	points: 300;
-	scale: .45;
-	x: cos(2t) + cos(7t);
-	y: sin(2t) + sin(7t);
+  fill-rule: evenodd;
+  points: 300;
+  scale: .45;
+  x: cos(2t) + cos(7t);
+  y: sin(2t) + sin(7t);
 
 
 #heart
-	fill-rule: evenodd;
-	points: 300;
-	scale: .45;
-	x: cos(4t) + cos(5t);
-	y: sin(4t) + sin(5t);
+  fill-rule: evenodd;
+  points: 300;
+  scale: .45;
+  x: cos(4t) + cos(5t);
+  y: sin(4t) + sin(5t);
 
 
 #holes
-	fill-rule: evenodd;
-	points: 200;
-	scale: .3;
-	x: 2 * sin(t) + sin(7t);
-	y: 2 * cos(t) + cos(7t);
+  fill-rule: evenodd;
+  points: 200;
+  scale: .3;
+  x: 2 * sin(t) + sin(7t);
+  y: 2 * cos(t) + cos(7t);
 
 
 #bird
-	points: 200;
-	scale: .3;
-	rotate: 30;
-	x: 2 * cos(3t) + cos(t);
-	y: 2 * sin(t) + sin(2t);
+  points: 200;
+  scale: .3;
+  rotate: 30;
+  x: 2 * cos(3t) + cos(t);
+  y: 2 * sin(t) + sin(2t);
 
 
 #doll
-	points: 200;
-	scale: .3;
-	x: sin(4t) + 2 * sin(t);
-	y: cos(3t) + 2 * cos(t);
+  points: 200;
+  scale: .3;
+  x: sin(4t) + 2 * sin(t);
+  y: cos(3t) + 2 * cos(t);
 
 
 #number eight
-	points: 200;
-	x: cos(t) * sin(6t);
-	y: sin(t) * cos(3t);
+  points: 200;
+  x: cos(t) * sin(6t);
+  y: sin(t) * cos(3t);
 
 
 #bag
-	points: 200;
-	scale: .35;
-	rotate: -90;
-	x: cos(t) - cos(2t) - cos(4t) + .5;
-	y: sin(2t) - sin(5t) - sin(t);
+  points: 200;
+  scale: .35;
+  rotate: -90;
+  x: cos(t) - cos(2t) - cos(4t) + .5;
+  y: sin(2t) - sin(5t) - sin(t);
 
 
 #music note
-	points: 300;
-	scale: .35;
-	rotate: 45;
-	x: sin(t) + cos(7t) + cos(2t) + .35;
-	y: cos(4t) + sin(4t) + sin(t);
+  points: 300;
+  scale: .35;
+  rotate: 45;
+  x: sin(t) + cos(7t) + cos(2t) + .35;
+  y: cos(4t) + sin(4t) + sin(t);
 
 
 #flower petal
-	fill-rule: evenodd;
-	points: 200;
-	scale: .6;
-	r: cos(5t)^2 + sin(5t) + .3;
+  fill-rule: evenodd;
+  points: 200;
+  scale: .6;
+  r: cos(5t)^2 + sin(5t) + .3;
 
 
 #hypershape
-	points: 360;
-	m: 5;
-	n: .3;
-	p1: abs.cos(t * m/4) ^ n;
-	p2: abs.sin(t * m/4) ^ n;
-	r:  1 / (p1 + p2) ^ (1 / n);
+  points: 360;
+  m: 5;
+  n: .3;
+  p1: abs.cos(t * m/4) ^ n;
+  p2: abs.sin(t * m/4) ^ n;
+  r:  1 / (p1 + p2) ^ (1 / n);
 
 
 #vase
-	points: 200;
-	scale: .3;
-	rotate: 180;
-	x: sin(4t) + sin(t) * 1.6;
-	y: cos(t) + cos(t) * 4.8 + .3;
+  points: 200;
+  scale: .3;
+  rotate: 180;
+  x: sin(4t) + sin(t) * 1.3;
+  y: cos(t) + cos(t) * 4.9 + .3;
 
 
 #popcorn
-	points: 400;
-	scale: .7;
-	frame: 40;
-	r: abs.cos(9t) ^ cos(9t);
+  points: 400;
+  scale: .7;
+  frame: 40;
+  r: abs.cos(9t) ^ cos(9t);
 
 
 #belt
-	points: 120;
-	scale: .7;
-	frame: 15;
-	r: cos(t/10) ^ sin(t) ^ sin(2t);
+  points: 120;
+  scale: .7;
+  frame: 15;
+  r: cos(t/10) ^ sin(t) ^ sin(2t);
 
 
 #squircle
-	/* Squircle */
+  /* Squircle */
 
-	points: 100;
-	scale: .7;
-	r: 1/(1-.5*sin(2t)^2)^.25;
+  points: 100;
+  scale: .7;
+  r: 1/(1-.5*sin(2t)^2)^.25;
 
 
 #pattern
